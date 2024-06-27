e90f8d2ff675e55a19adf50223c0bfac4f9ff1ce
Wendy Donley Pierre <155206385+Wendy0206@users.noreply.github.com>
2024-06-27 03:05:43 +0000

    new link added

--- main.ts
+++ main.ts
@@ -1,170 +1,144 @@
 
 
-window.onload = function () {
+window.onload = function() {
+  console.log('lets see this file is called')
+  const menuOff = document.querySelectorAll(".link_section") as NodeListOf<HTMLDivElement>;
+  const submit = document.querySelector(".submit-btn") as HTMLButtonElement;
+  const darkMode = document.querySelector(".darkMode") as HTMLElement;
+  const imageHover = document.querySelectorAll(".project_img_div") as NodeListOf<HTMLDivElement>;
+  const showInfo = document.querySelectorAll(".project_info_div") as NodeListOf<HTMLDivElement>;
 
-  const menu_off = document.querySelectorAll<HTMLDivElement>(".link_section");
-  const submit = document.querySelector(".submit-btn") as HTMLElement | null;
-  const dark_mode: HTMLElement | null = document.querySelector(".darkMode");
-  const image_hover: NodeListOf<Element> = document.querySelectorAll(".project_img_div");
-  const show_info: NodeListOf<Element> = document.querySelectorAll(".project_info_div");
+  imageHover[0].addEventListener("mouseover", () => {
+    console.log('we hover the first one lets see');
+    showInfo[0].classList.add("show_project_info");
+    showInfo[1].classList.remove("show_project_info");
+    showInfo[2].classList.remove("show_project_info");
+  });
 
-  image_hover[0].addEventListener("mouseover", ()=> {  
-    console.log('I detect this piece of code works')
-          show_info[0].classList.add("show_project_info");
-          show_info[1].classList.remove("show_project_info");
-          show_info[2].classList.remove("show_project_info"); 
-        });
-    
-    image_hover[1].addEventListener("mouseover", ()=> {  
-      show_info[1].classList.add("show_project_info");
-      show_info[0].classList.remove("show_project_info");
-      show_info[2].classList.remove("show_project_info");  
+  imageHover[1].addEventListener("mouseover", () => {
+    showInfo[1].classList.add("show_project_info");
+    showInfo[0].classList.remove("show_project_info");
+    showInfo[2].classList.remove("show_project_info");
+  });
+
+  imageHover[2].addEventListener("mouseover", () => {
+    showInfo[2].classList.add("show_project_info");
+    showInfo[0].classList.remove("show_project_info");
+    showInfo[1].classList.remove("show_project_info");
+  });
+
+  imageHover[3].addEventListener("mouseover", () => {
+    showInfo[3].classList.add("show_project_info");
+    showInfo[4].classList.remove("show_project_info");
+    showInfo[5].classList.remove("show_project_info");
+  });
+
+  imageHover[4].addEventListener("mouseover", () => {
+    showInfo[4].classList.add("show_project_info");
+    showInfo[3].classList.remove("show_project_info");
+    showInfo[5].classList.remove("show_project_info");
+  });
+
+  imageHover[5].addEventListener("mouseover", () => {
+    showInfo[5].classList.add("show_project_info");
+    showInfo[3].classList.remove("show_project_info");
+    showInfo[4].classList.remove("show_project_info");
+  });
+
+  for (let i = 0; i < imageHover.length; i++) {
+    showInfo[i].addEventListener("mouseleave", () => {
+      showInfo[i].classList.remove("show_project_info");
     });
-
-    image_hover[2].addEventListener("mouseover", ()=> {  
-      show_info[2].classList.add("show_project_info");
-      show_info[0].classList.remove("show_project_info");
-      show_info[1].classList.remove("show_project_info");
-    });
-
-
-  for (let i = 0; i < image_hover.length; i++) {
-    if (image_hover[i] instanceof HTMLElement && show_info[i] instanceof HTMLElement) {
-     show_info[i].addEventListener("mouseleave", ()=> { 
-        show_info[i].classList.remove("show_project_info");
-        
-      });
-    }
   }
 
-
-
-  menu_off.forEach((link: HTMLDivElement) => {
-    link.addEventListener('click', () => {
-      const uncheck = document.querySelector<HTMLInputElement>("#ham-menu");
+  menuOff.forEach(link => {
+    link.addEventListener("click", () => {
+      const uncheck = document.querySelector("#ham-menu") as HTMLInputElement;
       if (uncheck) {
         uncheck.checked = false;
       }
     });
   });
-  
-  const currentYear: number = new Date().getFullYear();
-  const footer_year: HTMLElement | null = document.querySelector(".footer_text");
-  if (footer_year !== null) {
-    footer_year.innerHTML = `© ${currentYear} wendydonleypierre.dev`;
+
+  const currentYear = new Date().getFullYear();
+  const footerYear = document.querySelector(".footer_text") as HTMLElement | null;
+  if (footerYear !== null) {
+    footerYear.innerHTML = `© ${currentYear} wendydonleypierre.dev`;
   }
 
+  let currentMode = 1;
+  darkMode.addEventListener("click", () => {
+    const homeLeft = document.querySelector(".left_side") as HTMLElement | null;
+    const project = document.querySelectorAll(".all_project_div") as NodeListOf<HTMLDivElement>;
+    const footerS = document.querySelector(".footer_style") as HTMLElement | null;
+    const container = document.querySelector(".container_div") as HTMLElement | null;
+    const awesome = document.querySelectorAll(".about_img i") as NodeListOf<HTMLElement>;
+    const inputBg = document.querySelectorAll(".input_class") as NodeListOf<HTMLDivElement>;
+    const chat = document.querySelector(".contact_text") as HTMLElement | null;
+    const social = document.querySelectorAll(".social_a") as NodeListOf<HTMLAnchorElement>;
+    const hiLine = document.querySelectorAll(".line_hi") as NodeListOf<HTMLDivElement>;
 
-  let current_mode: number = 1;
-  if (dark_mode) {
-   
-    dark_mode.addEventListener("click", () => {
-      console.log('cikwnkwnk')
-      const home_left = document.querySelector(".left_side") as HTMLElement | null;
-      const project = document.querySelector(".project_div") as HTMLElement | null;
-      const footer_s = document.querySelector(".footer_style") as HTMLElement | null;
-      const container = document.querySelector(".container_div") as HTMLElement | null;
-      const awesome = document.querySelectorAll(".about_img i")  as NodeListOf<HTMLElement>;
-      const input_bg = document.querySelectorAll(".input_class") as NodeListOf<HTMLElement>;
-      const chat = document.querySelector(".contact_text") as HTMLElement | null;
-      const social = document.querySelectorAll(".social_a") as NodeListOf<HTMLElement>;
-      const hi_line = document.querySelectorAll(".line_hi") as NodeListOf<HTMLElement>;
-  
-  
-        if (home_left && project && footer_s && submit && container && footer_year && hi_line && chat && social) {
-          if (current_mode == 1) {
-          home_left.style.backgroundColor = "#1a261d";
-          project.style.backgroundColor = "#1a261d";
-          footer_s.style.backgroundColor = "#1a261d";
-          submit.style.backgroundColor = "#1a261d";
-          footer_year.style.color = "white";
-       
-          chat.style.color = "white";
-          social[0].style.color = "white";
-          social[1].style.color = "white";
-          social[2].style.color = "white";
-          hi_line[0].style.color = "white";
-          hi_line[1].style.color = "white";
-          container.classList.toggle("dark_mode");
-          input_bg.forEach((element: HTMLElement) => {
-            element.style.backgroundColor = "rgba(33, 96, 47, 0.51)";
-          });
+    if (currentMode === 1) {
+      darkMode.classList.remove("fa-moon");
+      darkMode.classList.add("fa-sun");
+      if (homeLeft) homeLeft.style.backgroundColor = "#1a261d";
+      project[0].style.backgroundColor = "#1a261d";
+      project[1].style.backgroundColor = "#1a261d";
+      if (footerS) footerS.style.backgroundColor = "#1a261d";
+      submit.style.backgroundColor = "#1a261d";
+      if (footerYear) footerYear.style.color = "white";
 
-          awesome.forEach((element: HTMLElement) => {
-            element.style.color = "white";
-          });
-          document.documentElement.style.setProperty("--color", "white");
-          current_mode = 0;
-        }
-       else {
-   
+      if (chat) chat.style.color = "white";
+      social.forEach(element => element.style.color = "white");
+      hiLine.forEach(element => element.style.color = "white");
+      if (container) container.classList.toggle("dark_mode");
+      inputBg.forEach(element => {
+        element.style.backgroundColor = "rgba(33, 96, 47, 0.51)";
+      });
 
-          home_left.style.backgroundColor = "rgba(33, 96, 47, 0.51)";
-          project.style.backgroundColor = "rgba(33, 96, 47, 0.51)";
-          footer_s.style.backgroundColor = "rgba(33, 96, 47, 0.51)";
-          submit.style.backgroundColor = "rgba(33, 96, 47, 0.51)";
-          
-          chat.style.color = "#1a261d";
-          footer_year.style.color = "black";
-          container.classList.toggle("dark_mode");
-          awesome.forEach((element: HTMLElement) => {
-            element.style.color = "black";
-          });
-          hi_line[0].style.color = "black";
-          hi_line[1].style.color = "black";
-          social[0].style.color = "black";
-          social[1].style.color = "black";
-          social[2].style.color = "black";
-          input_bg.forEach((element: HTMLElement) => {
-            element.style.backgroundColor = "#2d2d2d";
-          });
-          document.documentElement.style.setProperty("--color", "#373B44");
-          current_mode = 1;
-        }
-      }
-    });
-  }
+      awesome.forEach(element => {
+        element.style.color = "white";
+      });
+      document.documentElement.style.setProperty("--color", "white");
+      currentMode = 0;
+    } else {
+      darkMode.classList.remove("fa-remove");
+      darkMode.classList.add("fa-moon");
+      if (homeLeft) homeLeft.style.backgroundColor = "rgba(33, 96, 47, 0.51)";
+      project[0].style.backgroundColor = "rgba(33, 96, 47, 0.51)";
+      project[1].style.backgroundColor = "rgba(33, 96, 47, 0.51)";
+      if (footerS) footerS.style.backgroundColor = "rgba(33, 96, 47, 0.51)";
+      submit.style.backgroundColor = "rgba(33, 96, 47, 0.51)";
 
-
-
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY + window.innerHeight >= document.documentElement.scrollHeight) {
-   const reveals= document.querySelectorAll<HTMLElement>('.reveal') as NodeListOf<HTMLElement>;
-   for (let i = 0; i < reveals.length; i++) {
-    reveals[i].classList.add('active');
-   }
-
-  }
-});
-
-
-if(submit){
-  submit.addEventListener("click", () => {
-  const comment = document.querySelector(".comment_in")  as HTMLInputElement | null;
-  const phone = document.querySelector(".phone_in")  as HTMLInputElement | null;
-const email = document.querySelector(".email_in")  as HTMLInputElement | null;
-
-  if(phone && email && comment ){
-    let email_val: string = email.value;
-  
-    let regex: RegExp = /\D/;
-    let bool: boolean = regex.test(phone.value);
-    if (!email_val.includes("@")) {
-        email.classList.add('is-invalid');
+      if (chat) chat.style.color = "#1a261d";
+      if (footerYear) footerYear.style.color = "black";
+      if (container) container.classList.toggle("dark_mode");
+      awesome.forEach(element => {
+        element.style.color = "black";
+      });
+      hiLine.forEach(element => element.style.color = "black");
+      social.forEach(element => element.style.color = "black");
+      inputBg.forEach(element => {
+        element.style.backgroundColor = "#2d2d2d";
+      });
+      document.documentElement.style.setProperty("--color", "#373B44");
+      currentMode = 1;
     }
-   
-    if (bool || phone.value.length<7) {
-      phone.classList.add('is-invalid');
-    }
-  
-  }
-    
   });
-}
 
+  window.addEventListener("scroll", () => {
+    if ((window.scrollY + window.innerHeight)== document.body.scrollHeight) {
+      console.log('hey we get got the bottom');
+      const reveals = document.querySelectorAll(".reveal") as NodeListOf<HTMLElement>;
+      reveals.forEach(element => {
+        element.classList.add("active");
+      });
+    }
+  });
 
-
-
-
-}
+  submit.addEventListener("click", () => {
+    // const comment = document.querySelector(".comment_in") as HTMLInputElement | null;
+    // const phone = document.querySelector(".phone_in") as HTMLInputElement | null;
+    // const email = document.querySelector(".email_in") as HTMLInputElement | null;
+  });
+};
